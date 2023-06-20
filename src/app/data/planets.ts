d1fecf579054fe8480efc77c0fd1a353c42a77c9
Daniel Vieira <dann-vieira@hotmail.com>
2023-06-20 10:46:05 -0300

    add planets data source

---
+++ src/app/data/planets.ts
@@ -0,0 +1,10 @@
+export const PLANETS = [
+  {name: 'Mercúrio', image: '/images/Mercurio.png', x: 32, y: 32},
+  {name: 'Vênus', image: '/images/Venus.png', x: 64, y: 64},
+  {name: 'Terra', image: '/images/Terra.png', x: 64, y: 64},
+  {name: 'Marte', image: '/images/Marte.png', x: 48, y: 48},
+  {name: 'Júpiter', image: '/images/Jupiter.png', x: 220, y: 200, css: "jupiter"},
+  {name: 'Saturno', image: '/images/Saturno.png', x: 252, y: 180, css: 'saturn'},
+  {name: 'Urano', image: '/images/Urano.png', x: 96, y: 96, css: 'uranus'},
+  {name: 'Netuno', image: '/images/Netuno.png', x: 92, y: 92, css: 'neptune'},
+];
