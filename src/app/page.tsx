fa200a6dbfd8ed555aff079eb65174f6b6ff9394
Daniel Vieira <dann-vieira@hotmail.com>
2023-06-20 10:49:04 -0300

    render components on the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Header from "./components/Header";
+import Missions from "./components/Missions";
+import SolarSystem from "./components/SolarSystem";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col items-center">
+      <Header />
+      <div>
+        <SolarSystem />
+        <Missions />
+      </div>
+    </main>
+  );
 }
